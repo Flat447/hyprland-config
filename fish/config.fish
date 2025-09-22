8f4e1957aefb9bf60c61165ae4122b9fb57b09f8
FLAT447 <alexey_mezentsev@list.ru>
2025-09-22 17:05:23 +0500

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,19 +1,20 @@
 if status is-interactive
-fastfetch
+fastfetch -c ~/.config/fastfetch/mini_conf.jsonc
 echo -e "\033[33m davay eBash"
     # Commands to run in interactive sessions can go here
 end
-alias ls="ls -Aa"
+alias ls="lsd -Aa"
 alias ff="fastfetch"
 alias zshconfig="vim ~/.zshrc"
 alias v="vim"
+alias g="git"
+alias mfetch="fastfetch -c ~/.config/fastfetch/mini_conf.jsonc"
 alias update="sudo pacman -Syu"
-alias sysfetch="/home/flat/.sysfetch.sh"
-alias fetch="/home/flat/.fetch.sh"
+alias sysfetch="~/.config/hypr/.sysfetch.sh"
+alias fetch="~/.config/hypr/.fetch.sh"
 alias wallpaper-changer="/home/flat/.config/rofi/wallpapermenu.sh"
 set fish_greeting ""
 alias hyprcfg="nvim ~/.config/hypr/hyprland.conf"
 alias tomas="sl"
 alias n="nvim"
 alias webcam="mpv av://v4l2:/dev/video0"
-set -gx PATH /usr/lib/flutter/bin $PATH
